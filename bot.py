5e33b6541e181e15ad8c3babfcbd38fc6c22b558
ulhangry <80541020+umjiwan@users.noreply.github.com>
2021-12-16 12:45:12 +0900

    코드 간소화 🤔

--- bot.py
+++ bot.py
@@ -2,10 +2,11 @@ import discord
 import asyncio
 import os.path
 import sys
+from discord.ext import commands
 import lelle
 
-
-client = discord.Client()
+ing = discord.Activity(type=discord.ActivityType.listening ,name="u도움말")
+client = commands.Bot(status=discord.Status.online, activity=ing, command_prefix="u")
 
 if os.path.isfile("token.txt"):
     pass
@@ -25,39 +26,28 @@ f = open("token.txt", "r+")
 token = str(f.read())
 f.close()
 
-@client.event
-async def on_ready():
-    print("login!")
-    print(client.user.name)
-    print(client.user.id)
-    print("-"*15)
+@client.command(aliases=["주식"])
+async def stock(ctx, *, s_name:str):
+    stock = lelle.stock(s_name)
+    name = stock.name()
+    price = stock.price()
+    compare = stock.compare()
+    fluctuating = stock.fluctuating()
+    sellask = stock.sellask()
+    buyask = stock.buyask()
+    transaction = stock.transaction()
 
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return None
-    if message.content.startswith("u"):
-        msg = message.content[1:]
-    else:
-        return None
+    await ctx.channel.send(name)
+    await ctx.channel.send(price)
+    await ctx.channel.send(compare)
+    await ctx.channel.send(fluctuating)
+    await ctx.channel.send(sellask)
+    await ctx.channel.send(buyask)
+    await ctx.channel.send(transaction)
 
+@stock.error
+async def stock_error(ctx, error):
+    await ctx.channel.send("주식 <종목명> 양식에 맞게 입력하여주세요.")
 
-    if msg[:2] == "주식":
-        stock = lelle.stock(msg[3:])
-        name = stock.name()
-        price = stock.price()
-        compare = stock.compare()
-        fluctuating = stock.fluctuating()
-        sellask = stock.sellask()
-        buyask = stock.buyask()
-        transaction = stock.transaction()
-
-        await message.channel.send(name)
-        await message.channel.send(price)
-        await message.channel.send(compare)
-        await message.channel.send(fluctuating)
-        await message.channel.send(sellask)
-        await message.channel.send(buyask)
-        await message.channel.send(transaction)
 
 client.run(token)
