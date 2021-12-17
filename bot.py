6c7aee4c90af7c9bc5395575ca093a4a56633eab
ulhangry <80541020+umjiwan@users.noreply.github.com>
2021-12-17 14:28:47 +0900

    1차 완성 🖌

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from typing import Text
 import discord
 import asyncio
 import os.path
@@ -5,7 +6,7 @@ import sys
 from discord.ext import commands
 import lelle
 
-ing = discord.Activity(type=discord.ActivityType.listening ,name="u도움말")
+ing = discord.Activity(type=discord.ActivityType.listening, name="u도움말")
 client = commands.Bot(status=discord.Status.online, activity=ing, command_prefix="u")
 
 if os.path.isfile("token.txt"):
@@ -30,24 +31,22 @@ f.close()
 async def stock(ctx, *, s_name:str):
     stock = lelle.stock(s_name)
     name = stock.name()
+    code = stock.code()
     price = stock.price()
     compare = stock.compare()
-    fluctuating = stock.fluctuating()
-    sellask = stock.sellask()
-    buyask = stock.buyask()
-    transaction = stock.transaction()
+    pe_compare = stock.pe_compare()
 
-    await ctx.channel.send(name)
-    await ctx.channel.send(price)
-    await ctx.channel.send(compare)
-    await ctx.channel.send(fluctuating)
-    await ctx.channel.send(sellask)
-    await ctx.channel.send(buyask)
-    await ctx.channel.send(transaction)
+    embed = discord.Embed(title=(f"{name}  |  {code}"), color=0x99ddff)
+    embed.add_field(name="현재가", value=(f"{price}원"), inline=True)
+    embed.add_field(name="등락율", value=pe_compare, inline=True)
+    embed.add_field(name="전일대비", value=compare, inline=True)
+    embed.set_footer(text=("Source by : "+ "https://finance.naver.com/item/main.naver?code=" + code))
 
-@stock.error
+    await ctx.channel.send(embed=embed)
+
+"""@stock.error
 async def stock_error(ctx, error):
-    await ctx.channel.send("주식 <종목명> 양식에 맞게 입력하여주세요.")
+    await ctx.channel.send("주식 <종목명> 양식에 맞게 입력하여주세요.")"""
 
 
 client.run(token)
