24cf055c43e3d7d76302f1b65e829b0869644f3f
ulhangry <80541020+umjiwan@users.noreply.github.com>
2021-12-18 13:44:12 +0900

    도움말 기능 추가 시도 🤔

--- bot.py
+++ bot.py
@@ -26,6 +26,22 @@ f = open("token.txt", "r+")
 token = str(f.read())
 f.close()
 
+@client.command(aliases=["도움말"])
+async def lelle_help(ctx, *, name:str):
+    if name == "":
+        embed = discord.Embed(color=0xffffff)
+        embed.set_author(name="lelle  |  도움말")
+        embed.add_field(name="소개", value="디스코드를 더 편리하게, 크롤링 기반 렐레봇", inline=False)
+        embed.add_field(name="도움말 명령어", inline=False)
+        embed.add_field(name="u도움말 주식", value="주식 명령어 사용법을 알려준다.", inline=True)
+        
+        await ctx.channel.send(embed=embed)
+ 
+
+    elif name == "주식":
+        pass
+
+
 @client.command(aliases=["주식"])
 async def stock(ctx, *, s_name:str):
     stock = lelle.stock(s_name)
@@ -35,7 +51,7 @@ async def stock(ctx, *, s_name:str):
     compare = stock.compare()
     pe_compare = stock.pe_compare()
 
-    embed = discord.Embed(title=(f"{name}  |  {code}"), color=0x99ddff)
+    embed = discord.Embed(title=(f"{name}  |  {code}"), color=0xffffff)
     embed.add_field(name="현재가", value=(f"{price}원"), inline=True)
     embed.add_field(name="등락율", value=pe_compare, inline=True)
     embed.add_field(name="전일대비", value=compare, inline=True)
