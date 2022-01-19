1c4de35c908f2b4fa9836b61e555c132ad29d9aa
ulhangry <80541020+umjiwan@users.noreply.github.com>
2022-01-19 22:29:41 +0900

    test

--- bot.py
+++ bot.py
@@ -41,30 +41,7 @@ class SpecialMemberConverter(Converter): # 출처 https://github.com/janu8ry/kku
     async def convert(self, ctx: Context, argument: str) -> Union[discord.User, discord.Member]:
         argument = argument.lstrip()
 
-        if not argument:
-            return ctx.author
-
-        try:
-            return await MemberConverter().convert(ctx, argument)
-        except errors.MemberNotFound:
-            pass
-        try:
-            return await UserConverter().convert(ctx, argument)
-        except errors.UserNotFound:
-            pass
-
-        if argument.isdecimal():  # if argument is user id
-            user = await ctx.bot.db.user.find_one({'_id': int(argument)})
-            return await ctx.bot.fetch_user(user['_id'])
-        else:
-            if re.match(r"<@!?([0-9]+)>$", argument):  # if argument is mention
-                return await ctx.bot.fetch_user(int(re.findall(r'\d+', argument)[0]))
-            else:
-                user = await ctx.bot.db.user.find_one({'_name': str(argument)})
-                if user:  # if argument is user name
-                    return await ctx.bot.fetch_user(user['_id'])
-                else:
-                    raise errors.BadArgument
+        return ctx.author.id
 
 
 @client.command(aliases=["도움말"])
