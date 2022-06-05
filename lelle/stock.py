b2ee05bda3b2b0308868bf89463204c48e93d3c9
ulhangry <80541020+umjiwan@users.noreply.github.com>
2022-06-05 20:22:41 +0900

    remake lelle.py

---
+++ lelle/stock.py
@@ -0,0 +1,59 @@
+from urllib import parse
+from bs4 import BeautifulSoup
+import requests
+
+class url:
+    def __init__(self, stock: str):
+        self.stock = stock
+        self.BASE_URL = "https://finance.naver.com"
+
+    def convert_stock(self):
+        return parse.quote(self.stock.encode("euc-kr"))
+
+    def get(self):
+        search_url = self.BASE_URL + "/search/searchList.naver?query=" + self.convert_stock()
+        search_response = requests.get(search_url)
+        search_html = search_response.text
+        search_soup = BeautifulSoup(search_html, "html.parser")
+        search_selector = "#content > div.section_search > table > tbody > tr:nth-child(1) > td.tit > a"
+
+        return self.BASE_URL + str(search_soup.select_one(search_selector).get("href"))
+
+class data:
+    def __init__(self, stock: str):
+        self.url = url(stock).get()
+        self.response = requests.get(self.url)
+        self.html = self.response.text
+        self.soup = BeautifulSoup(self.html, "html.parser")
+
+    def get_name(self):
+        selector = "#middle > div.h_company > div.wrap_company > h2 > a"
+        return self.soup.select_one(selector).text
+
+    def get_price(self, convert: bool=False):
+        selector = "#chart_area > div.rate_info > div > p > em > span.blind"
+        price = self.soup.select_one(selector).text
+
+        return price if not convert else int(price.replace(",", ""))
+
+    def get_yesterday_price(self, convert: bool=False):
+        selector = "#chart_area > div.rate_info > table > tr > td.first > em > span.blind"
+        yesterday_price = self.soup.select_one(selector).text
+
+        return yesterday_price if not convert else int(yesterday_price.replace(",", ""))
+
+    def get_change(self, percent: bool=False):
+        price = self.get_price(convert=True)
+        yesterday_price = self.get_yesterday_price(convert=True)
+        compare = price - yesterday_price
+
+        return compare if not percent else round(compare / yesterday_price * 100, 2)
+        
+if __name__ == "__main__":
+    stock = data("삼성전자")
+    print(stock.get_name())
+    print(stock.get_price())
+    print(stock.get_yesterday_price())
+    print(stock.get_change())
+    print(stock.get_change())
+    print(stock.get_change(percent=True))
