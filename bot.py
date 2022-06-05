8d2d99767edf0eede3bc65c3960c20e346b6d4d2
ulhangry <80541020+umjiwan@users.noreply.github.com>
2022-06-05 22:08:06 +0900

    update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 from lelle import stock
+import os
 
 lelle = commands.Bot(
     command_prefix="t",
@@ -17,6 +18,11 @@ async def on_ready():
     print(f"name: {lelle.user.name}")
     print(f"id: {lelle.user.id}")
     print("-" * 10)
+
+# 명령 파일 가져오기
+for file in os.listdir("lelle/"):
+    if file[-3:] == ".py":
+        lelle.load_extension(f"lelle.{file[:-3]}")
 
 with open("data/token.txt", "r") as token_file:
     token = token_file.read()
